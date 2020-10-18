0a0afbf809b039cee316438590294ad9905bfe70
Peter Tran <tran.peter08@gmail.com>
2020-10-18 03:40:45 -0700

    created separate module for queries

--- js/queries.js
+++ js/queries.js
@@ -1,46 +1,55 @@
 const memo = {};
 
-function validateRule(rule, cart) {
-  const queries = {
-    findItem(params) {
-      return function (item) {
-        const keys = Object.keys(params);
+const queries = {
+  findItem(cart, params) {
+    const key = JSON.stringify(['find', params]);
 
-        for (let key of keys) {
-          if (item[key] !== params[key]) {
-            return false;
-          }
+    if (memo[key]) {
+      return memo[key];
+    }
+
+    const result = cart.find((item) => {
+      for (let key in params) {
+        if (item[key] !== params[key]) {
+          return false;
         }
-
-        return true;
-      };
-    },
-
-    itemInCart(params) {
-      const key = JSON.stringify(params);
-      if (memo[key]) {
-        return memo[key];
       }
 
-      const result = cart.find(this.findItem(params));
-      memo[key] = result;
-      return result;
-    },
+      return true;
+    });
 
-    itemsInCart(paramList) {},
-  };
+    memo[key] = result;
+    return result;
+  },
 
-  const evaluators = {
-    toExist(result) {
-      return result ? true : false;
-    },
-  };
+  itemInCart(cartData, params) {
+    return this.findItem(cartData.cart, params);
+  },
 
-  const { query, eval, queryParams, evalParams } = rule;
-  const result = queries[query](queryParams);
+  itemsInCart(cartData, params) {
+    const key = JSON.stringify(['filter', params]);
 
-  console.log({ result });
-  return evaluators[eval](result, evalParams);
-}
+    if (memo[key]) {
+      return memo[key];
+    }
 
-module.exports = validateRule;
+    const results = cartData.cart.filter((item) => {
+      for (let key in params) {
+        if (item[key] !== params[key]) {
+          return false;
+        }
+      }
+
+      return true;
+    });
+
+    memo[key] = results;
+    return results;
+  },
+
+  metadataField(cartData, params) {
+    return cartData.metadata;
+  },
+};
+
+module.exports = queries;
