5c14ac336c87cc57a7e095c6cb1bb1dfaa1bad3d
Peter Tran <tran.peter08@gmail.com>
2020-10-18 22:47:56 -0700

    added more evaluator methods

--- js/evaluators.js
+++ js/evaluators.js
@@ -1,22 +1,42 @@
 const evaluators = {
   toExist(result) {
-    return result ? true : false;
-  },
-
-  hasResultCount(result = [], params = { count: 0 }) {
-    return result.length === params.count;
+    return !!result;
   },
 
   equalTo(result, params) {
     if (!params) throw 'Params undefined!';
+
     for (let key in params) {
-      console.log(result[key], params[key]);
       if (result[key] !== params[key]) {
         return false;
       }
     }
 
     return true;
+  },
+
+  greaterThan(result, params) {
+    for (let key in params) {
+      return result[key] > params[key];
+    }
+  },
+
+  lessThan(result, params) {
+    for (let key in params) {
+      return result[key] < params[key];
+    }
+  },
+
+  hasResultCount(result, params) {
+    return result.length === params.count;
+  },
+
+  hasResultCountGreaterThan(result, params) {
+    return result.length > params.count;
+  },
+
+  hasResultCountLessThan(result, params) {
+    return result.length < params.count;
   },
 };
 
